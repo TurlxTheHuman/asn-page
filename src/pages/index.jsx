69febbde1dcd8cc2ce002fdeab8279d9443977fd
Turlx <contact@turlxthehuman.com>
2023-10-01 15:01:56 -0700

    colors

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -43,13 +43,13 @@ function Main() {
   return (
     <body>
       <div id='center'>
-        <a href='https://turlxthehuman.com'><img src='/assets/img/coolturtle.webp' className='turtle'/></a>
+        <a href='https://turlxthehuman.com'><img src='/assets/img/coolturtle.webp' className='turtle' alt='cool-turtle'/></a>
         <h1>AS49132</h1>
         <p>a personal development network</p>
         <div className='links'>
-          <a href='https://www.peeringdb.com/net/34140'>peeringdb.com</a>
-          <a href='https://bgp.he.net/AS49132'> bgp.he.net</a>
-          <a href='https://bgp.tools/as/49132'> bgp.tools</a>
+          <a href='https://www.peeringdb.com/net/34140' aria-label="peeringDB">peeringdb.com</a>
+          <a href='https://bgp.he.net/AS49132' aria-label="he.net"> bgp.he.net</a>
+          <a href='https://bgp.tools/as/49132' aria-label="bgp.tools"> bgp.tools</a>
         </div>
         <ul id='ixp'>
         {data && data.netixlan_set.map((set, index) => (
